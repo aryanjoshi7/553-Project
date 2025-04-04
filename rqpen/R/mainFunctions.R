30d4a6978727bac825cca2fc08f4fdb88720cd2c
micahwil <micahwil@umich.edu>
2025-04-04 16:44:34 -0400

    print statements again

--- rqpen/R/mainFunctions.R
+++ rqpen/R/mainFunctions.R
@@ -2156,6 +2156,8 @@ rq.group.pen <- function(x,y, tau=.5,groups=1:ncol(x), penalty=c("gLASSO","gAdLA
 						lambda=NULL,nlambda=100,eps=ifelse(nrow(x)<ncol(x),.05,.01),alg=c("huber","br"), 
 						a=NULL, norm=2, group.pen.factor=NULL,tau.penalty.factor=rep(1,length(tau)),
 						scalex=TRUE,coef.cutoff=1e-8,max.iter=5000,converge.eps=1e-4,gamma=IQR(y)/10, lambda.discard=TRUE,weights=NULL, ...){
+	print("calling group.pen ")
+	
 	penalty <- match.arg(penalty)
 	alg <- match.arg(alg)
 	
@@ -2271,20 +2273,24 @@ rq.group.pen <- function(x,y, tau=.5,groups=1:ncol(x), penalty=c("gLASSO","gAdLA
 	}
 
 	penalty.factor <- mapvalues(groups,seq(1,g),group.pen.factor)
-	
+	print("Line 2274 rq.group.pen")
 	if(penalty == "gLASSO"){
+		print("gAdLASSO case rq.group.pen")
 		return_val <- rq.glasso(x,y,tau,groups, lambda, group.pen.factor,scalex,tau.penalty.factor,max.iter,converge.eps,gamma,lambda.discard=lambda.discard,weights=weights,...)
 	} else{
 		if(penalty == "gAdLASSO"){
+			print("gAdLASSO case rq.group.pen 2")
 			init.model <- rq.enet(x,y,tau,lambda=lambda,penalty.factor=penalty.factor,scalex=scalex,tau.penalty.factor=tau.penalty.factor,
 									a=0,max.iter=max.iter,converge.eps=converge.eps,gamma=gamma,lambda.discard=lambda.discard,...)
 		} else{
 			if(norm == 1){
-			  init.alg <- alg
+				print("norm 1 case rq.group.pen")
+			  	init.alg <- alg
 				init.model <- rq.lasso(x,y,tau,alg=init.alg,lambda=lambda,penalty.factor=penalty.factor,scalex=scalex,
 							tau.penalty.factor=tau.penalty.factor,max.iter=max.iter,coef.cutoff=coef.cutoff,converge.eps=converge.eps,
 							gamma=gamma,lambda.discard=lambda.discard,weights,...)
 			} else{
+				print("norm 2 case rq.group.pen")
 				init.model <- rq.glasso(x,y,tau,groups, lambda, group.pen.factor,scalex,tau.penalty.factor,max.iter,converge.eps,gamma,lambda.discard=lambda.discard,weights=weights,...)
 			}
 		}
